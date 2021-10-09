a86982ffa335de17fd4186f874fa77b63fc85fea
Fetah .OK <32603079+fetahokey@users.noreply.github.com>
2021-10-09 12:48:26 +0100

    session model

---
+++ src/model/session.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// Interface
+export interface SessionDocument extends mongoose.Document {
+
+}
+
+// Schema
+/*const SessionSchema = mongoose.Schema({
+
+});
+*/
