bffbcf21aa6031d8e5f9bb2859e1e1656d01cf1b
Fetah .OK <32603079+fetahokey@users.noreply.github.com>
2021-10-09 12:44:41 +0100

    set up default config file default.js

---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default {
+    port: 5000,
+    host: "localhost",
+    dbUri: "mongodb+srv://ecom:upLhwdCxibTSeoIz@cluster0.w4epm.mongodb.net/ecom-api?retryWrites=true&w=majority",
+    saltFactor: 10
+};
